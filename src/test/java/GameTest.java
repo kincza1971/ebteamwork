6d4961a03fad970ea722deaf3892e82ef853e79f
kincza <kincza@gmail.com>
2021-06-15 16:24:29 +0200

    Game test getWinner ready, passed

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -18,5 +20,33 @@ class GameTest {
 
     @Test
     void getSecondCountryScore() {
+    }
+
+    @Test
+    void getWinner() {
+        Game g1 = new Game("Mexico", "Brazil", 2,3);
+        Game g2 = new Game("Holland", "France",1,3);
+        Game g3 = new  Game("Brazil", "France",4,4);
+
+        Optional result = g1.getWinner();
+        if (result.isPresent()) {
+            assertEquals(result.get(),"Brazil");
+        } else {
+            fail();
+        }
+
+        result = g2.getWinner();
+
+        if (result.isPresent()) {
+            assertEquals(result.get(),"France");
+        } else {
+            fail();
+        }
+
+        result = g3.getWinner();
+
+        assertFalse(result.isPresent());
+
+
     }
 }
